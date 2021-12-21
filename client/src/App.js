959234b0f125d253c53f39b5b0b57fa6d224fd0e
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-12-21 23:26:16 +0000

    Tidy up files, Update design

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import icon from './icon.svg';
 import T9Form from './components/T9Form';
 import Results from './components/Results';
 
@@ -9,6 +10,15 @@ export default function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <p style={{ color: 'white' }}>Follow the phone down ;)</p>
+        <h1>
+          <img src={icon} alt='brick phone' />
+          TranslaT9r
+        </h1>
+        <h4 style={{ marginTop: '1%' }}>
+          Give us your T9 Number, and we'll give you a list of all the possible
+          words you could make!
+        </h4>
         <T9Form />
         {loading ? console.log('results still loading') : <Results />}
       </header>
